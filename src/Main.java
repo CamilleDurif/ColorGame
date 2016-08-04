5dabb4739da0a0f5880574f25898a8ce87671dbb
Your Name <you@example.com>
2016-08-04 06:05:11 +0200

    3/8

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 /*
  * The ColorGame application is a simple game based on a board of colored squares
- * and the goal is to fill the boar with only one color.
+ * where the goal is to fill the boar with only one color.
  * This program was made from zero and inspired by a mobile game.
  * This game implements new features : the choice of difficulty and a multiplayer mode.
  */
